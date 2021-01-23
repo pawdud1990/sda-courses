6fac1c683b6dfa5703d235bc405d9277f77483f2
Pawel Dudko <paweldudko@outlook.com>
2021-01-23 12:24:02 +0100

    first commit

---
+++ ZdJavaPol79JavaWprowadzenie/src/pl/sdacademy/java/wprowadzenie/HelloWorld.java
@@ -0,0 +1,12 @@
+package pl.sdacademy.java.wprowadzenie;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String greetings = "Hello world!";
+        System.out.println(greetings);
+
+        greetings = "Witaj!";
+        System.out.println(greetings);
+    }
+}
